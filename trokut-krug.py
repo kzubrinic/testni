af87ef08cc2dd91468e0c3f6a7065d74f43fc142
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2019-01-13 15:53:45 +0100

    Izmjena trokut-krug.py

--- trokut-krug.py
+++ trokut-krug.py
@@ -4,12 +4,12 @@ lik = input('Lik (T/K): ')
 str_pol = int(input('Stranica/polumjer: '))
               
 if lik == 'T':
-    ops = '3' * str_pol
+    ops = 3 * str_pol
     pov = 1.73 * str_pol ** 2 / 4
     lik_tekst = 'trokuta'
 else:
     ops = 2 * str_pol * 3.14
     pov = str_pol ** 2 * 3.14
-    lik_tekst = 'kruga
+    lik_tekst = 'kruga'
 
 print('Opseg',lik_tekst,'je',ops,', a površina',pov )
