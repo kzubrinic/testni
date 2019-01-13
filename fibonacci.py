743db8b2b457dcb373236a83cd0f13e5cff9357e
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2019-01-13 15:12:06 +0100

    Prvi commit

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+# Program koji računa Fibonaccijev niz
+# Pronađite i ispravite sintaksne greške
+
+N = int(input('Upiši broj članova niza: '))
+if N == 0:
+    print('0')
+else if N == 1:
+  print('0 1')
+else:
+    print('0 1', end=' ')
+    a, b = 0, 1
+    for i in range(0, N + 1):
+        c = a + b
+        a, b = b, c
+        printf(c, end = ' ')
